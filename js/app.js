7056c86707135be90551c267ded3f4b4f74f1124
Ruban <80041905+RubanN@users.noreply.github.com>
2021-06-23 21:33:12 +0530

    First Chapter is done by READING

---
+++ js/app.js
@@ -0,0 +1,63 @@
+
+// The First chapter HEAD FIRST BOOK
+
+//Test for jokes
+// var joke = "Javascript walked into a bar....."
+// var toldJoke = 'false';
+// var $punchline = 'Better watch out those semi-colors';
+// var ebtahe = 20;
+// var result;
+
+// if(toldJoke ==true){
+//     alert($punchline)
+// }else{
+//     alert(joke)
+// }
+// Movie Night
+// var code = 98104;
+// var orange =12;
+// if(!orange){
+//     console.log('there is no orange');
+// }else{
+//     console.log('yes there is some orange');
+// }
+
+
+//How the while loop works:
+// var scoops =1;
+// while(scoops>0){
+//     document.write("Another scoop!<br>");
+//     scoops = scoops-1;
+// }
+// document.write("Life without icecream is not the same!<br>");
+// if(scoops <3){
+//     alert("ice cream is runing low")
+// }
+// if(scoops >=5){
+//     alert("Eat faster, the ice is going to melt")
+// }else if(scoops === 3){
+//     alert("ice cream is running low")
+// }else if(scoops ===2){
+//     alert("ice cream is two")
+// }else if(scoops ===1){
+//     alert("ice cream is one")
+// }else{
+//     alert("Still lots of ice cream left, come and get it")
+// }
+
+//Coding a Serious Javascript Application
+
+// var word ="bottles";
+// var count =99;
+// while(count>0){
+//     console.log(count+""+word+"of beer on the wall");
+//     console.log(count+""+word+"of beer");
+//     console.log("Take one down , pass it around");
+//     count = count-1;
+//     if(count>0){
+//         console.log(count+""+word+"of beer on the wall");   
+//     }else{
+//         console.log("No more"+word+" of beer on the wall");
+//     }
+// }
+
